a6c845e819bedaef4f11259b30be35594b45e4cf
Abhisek Adhikari <abhisekadhikari1906@gmail.com>
2024-02-25 16:35:24 +0530

    Refactor appointment and requirements models

--- Model/appointent.js
+++ Model/appointent.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose")
 
-const appointmentSchema = mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
-  },
-  email: {
-    type: String,
-    require: true,
-  },
-  mobile: {
-    type: Number,
-    require: true,
-  },
-  service_type: {
-    type: String,
-    require: true,
-  },
-  message: {
-    type: String,
-    require: true,
-  },
-})
+const appointmentSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+        },
+        email: {
+            type: String,
+            require: true,
+        },
+        mobile: {
+            type: Number,
+            require: true,
+        },
+        service_type: {
+            type: String,
+            require: true,
+        },
+        message: {
+            type: String,
+            require: true,
+        },
+    },
+    { timestamps: true }
+)
 
 module.exports = new mongoose.model("appointment", appointmentSchema)
--- Model/requirements.js
+++ Model/requirements.js
@@ -1,46 +1,49 @@
 const mongoose = require("mongoose")
 
-const requirementSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    required: true,
-  },
-  site_details: {
-    type: String,
-    required: true,
-  },
-  area: {
-    type: Number,
-    required: true,
-  },
-  budget: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  services: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-})
+const requirementSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: Number,
+            required: true,
+        },
+        site_details: {
+            type: String,
+            required: true,
+        },
+        area: {
+            type: Number,
+            required: true,
+        },
+        budget: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        services: {
+            type: String,
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+)
 
 module.exports = new mongoose.model("requirements", requirementSchema)
